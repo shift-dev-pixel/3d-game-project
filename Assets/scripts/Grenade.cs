de20ea154215e4e89eb7b8e74e3d545618292800
shift-dev-pixel <120049140+shift-dev-pixel@users.noreply.github.com>
2024-11-01 13:53:39 +0300

    2

--- Assets/scripts/Grenade.cs
+++ Assets/scripts/Grenade.cs
@@ -1,18 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Grenade : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject grenadePrefab;
+    public Transform grenadeSpawn;
+    public float gremadeVelocity = 10f;
+    public float grenadePrefabLifetime = 10f;
+    public float grenadeCooldown = 3f;
+    private float lastUsedTime;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse1) && Time.time > lastUsedTime + grenadeCooldown)
+        {
+            fireWeapon();
+            lastUsedTime = Time.time;
+        }
+    }
+
+    private void fireWeapon()
+    {
+        GameObject grenade = Instantiate(grenadePrefab, grenadeSpawn.position, Quaternion.identity);
+
+        grenade.GetComponent<Rigidbody>().AddForce(grenadeSpawn.forward.normalized * gremadeVelocity, ForceMode.Impulse);
     }
 }
